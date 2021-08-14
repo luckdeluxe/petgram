735be30356adf1cc4f0c78b7c9db373f2b6a2701
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-14 10:30:52 -0300

    Add relation to the model and property

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from users.models import Profile
-
-# Create your models here.
 
 class Post(models.Model):
     """Post model."""
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
-
+    likes = models.ManyToManyField(User, blank=True, related_name='liked')
     title = models.CharField(max_length=255)
     photo = models.ImageField(upload_to='posts/photos')
     created = models.DateTimeField(auto_now_add=True)
@@ -20,3 +17,6 @@ class Post(models.Model):
         """Return title and username"""
         return f'{self.title} By @{self.user.username}'
     
+    @property
+    def num_likes(self):
+        return self.likes.all().count()
