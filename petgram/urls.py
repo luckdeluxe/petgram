03243cfd5c9fe37b1c3c649f6b31b4315bac0195
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-14 10:29:21 -0300

    Add urls to URLconf

--- petgram/urls.py
+++ petgram/urls.py
@@ -1,18 +1,4 @@
-"""platzigram URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""Petgram URL Configuration"""
 
 from users.views import ProfileSearchListView
 from django.conf import settings
@@ -22,16 +8,17 @@ from django.urls import path, include
 
 urlpatterns = [ 
 
-    path('search', ProfileSearchListView.as_view(), name='search'),
-
     path('admin/', admin.site.urls),
+
+    path('search/', ProfileSearchListView.as_view(), name='search'),
 
     path('', include(('posts.urls', 'posts'), namespace='posts')),
 
     path('users/', include(('users.urls', 'users'), namespace='users')),
 
-    path('comments', include(('comments.urls', 'comments'), namespace='comments')),
+    path('comments/', include(('comments.urls', 'comments'), namespace='comments')),
 
+    path('', include(('likes.urls', 'likes'), namespace='likes')),
 
 #Concatenate static with the values defined in settings.py
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
