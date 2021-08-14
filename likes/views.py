a75cf7a0128e43ae25b3882c476fedbda2afc9dd
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-14 10:26:45 -0300

    Add like method for posts

---
+++ likes/views.py
@@ -0,0 +1,31 @@
+#DJANGO
+from django.shortcuts import redirect
+
+#MODELS
+from posts.models import Post
+from likes.models import Like, LIKE_CHOICES
+
+
+def like_post(request):
+    user = request.user
+
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        post_obj = Post.objects.get(id=post_id)
+
+        if user in post_obj.likes.all():
+            post_obj.likes.remove(user)
+        else:
+            post_obj.likes.add(user)
+
+        like, created = Like.objects.get_or_create(user=user, post_id=post_id)
+
+
+        if not created:
+            if like.value == 'Like':
+                like.value = 'Unlike'
+            else:
+                like.value = 'Like'
+        like.save()
+
+    return redirect('posts:feed')
