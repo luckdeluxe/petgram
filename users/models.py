671ee666f7716c0ed81eb2c8a73563e901eecad1
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-20 22:16:38 -0300

    Add relationship for followers

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.auth.models import User
 
 # Makes email unique (User is class django default)
 User._meta.get_field('email')._unique = True
 
-# Create your models here.
 class Profile(models.Model):
     """Profile model.
         Proxy model that extends the base data with
@@ -21,6 +21,11 @@ class Profile(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    #Followers system
+    followers = models.ManyToManyField(User, related_name='is_following', blank=True) #user.followers.all()
+    #Status online-offline
+    activated = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
