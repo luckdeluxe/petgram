5f8e74f6c9c3bd5faae604c29ad9b4b6a86eb3f4
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-08-27 15:19:52 -0300

    Update environment variables

--- petgram/settings.py
+++ petgram/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%@xolig0r#%z49&t#zlej0c8va^@s(3j=hf-c12=ofvfq(s!q4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
